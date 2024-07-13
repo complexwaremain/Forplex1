3d6c18715c5613e27d392dd2cc9cbf9b0c1f8d8c
Complex <166940834+complexwaremain@users.noreply.github.com>
2024-07-13 13:16:32 +0400

    Add files via upload

--- GuiLibrary.lua
+++ GuiLibrary.lua
@@ -1,5 +1,3 @@
-
-
 --This watermark is used to delete the file if its cached, remove it to make the file persist after commits.
 if shared.VapeExecuted then
 	local VERSION = "4.10"..(shared.VapePrivate and " PRIVATE" or "").." "..readfile("vape/commithash.txt"):sub(1, 6)
@@ -147,7 +145,7 @@ if shared.VapeExecuted then
 	local vapeCachedAssets = {}
 	local function vapeGithubRequest(scripturl)
 		if not isfile("vape/"..scripturl) then
-			local suc, res = pcall(function() return game:HttpGet("https://raw.githubusercontent.com/complexwaremain/vape/"..readfile("vape/commithash.txt").."/"..scripturl, true) end)
+			local suc, res = pcall(function() return game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/"..readfile("vape/commithash.txt").."/"..scripturl, true) end)
 			assert(suc, res)
 			assert(res ~= "404: Not Found", res)
 			if scripturl:find(".lua") then res = "--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.\n"..res end
